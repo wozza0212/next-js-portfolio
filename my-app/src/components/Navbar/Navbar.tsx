b27a9014332634364ee0d0f41cd782cb2bc2f90d
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-06-15 20:52:36 +0100

    wip navbar

--- my-app/src/components/Navbar/Navbar.tsx
+++ my-app/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,27 @@
+"use client";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import Image from "next/image";
+import { use, useState } from "react";
 
 const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => setMobileMenu((mobileMenu) => !mobileMenu);
+
   return (
     <div className={styles.navComponent}>
       <nav>
+        <div className={styles.hamurgerIcon}>
+          <Image
+            className={styles.hamburger}
+            src="/menu.svg"
+            alt="HamburgerMenu"
+            width={40}
+            height={40}
+            priority
+            onClick={toggleMenu}
+          />
+        </div>
         <ul className={styles.navList}>
           <div className={styles.navLink}>
             <Link href={"/"}>Home</Link>
