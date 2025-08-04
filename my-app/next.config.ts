5c8b5cc5dc6061ec460fe745c9d7cd1e1296ab45
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-08-04 19:32:55 +0100

    display image

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "digimon.shadowsmith.com",
+        port: "",
+        pathname: "/img/**",
+        search: "",
+      },
+    ],
+  },
   /* config options here */
 };
+
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "/digimon.shadowsmith.com/",
+//         port: "",
+//         pathname: "/img/**",
+//         search: "",
+//       },
+//     ],
+//   },
+// };
 
 export default nextConfig;
