0504f30792944870f718c4d8c7d4f76a22b4fa6e
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-08-05 21:09:05 +0100

    own use fetch component

---
+++ my-app/src/app/react-projects/helpers/types.ts
@@ -0,0 +1,17 @@
+export interface DigiFetchObject {
+  url: string;
+  listLength: number;
+  initialValue: {};
+}
+
+export interface Digimon {
+  name?: string;
+  img?: string;
+  level?: string;
+}
+
+export interface RandomFetchObject {
+  url: string;
+  listLength: number
+  initialValue: {};
+}
--- my-app/src/app/react-projects/use-effect/page.tsx
+++ my-app/src/app/react-projects/use-effect/page.tsx
@@ -1,35 +1,24 @@
 "use client";
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useRandomFetch } from "../helpers/helpers";
+import { DigiFetchObject } from "../helpers/types";
 import Image from "next/image";
-
-interface Digimon {
-  name?: string;
-  img?: string;
-  level?: string;
-}
 
 const data = await fetch("https://digimon-api.vercel.app/api/digimon/").then(
   (response) => response.json()
 );
-const randomNum = data.length;
-
-const digiNumber = Math.floor(Math.random() * randomNum);
+const digiListLength = data.length;
 
 const endpoint: string = "https://digimon-api.vercel.app/api/digimon/";
 
 const useEffectProject: NextPage = () => {
-  const [digimon, setDigimon] = useState<Digimon>({});
-  useEffect(() => {
-    fetch(endpoint)
-      .then((response) => response.json())
-      .then((randomDigimon) => {
-        console.log("digimon", randomDigimon[digiNumber]);
-        setDigimon(randomDigimon[digiNumber]);
-      });
-  }, []);
+  const digiFetchValues = {
+    url: endpoint,
+    listLength: digiListLength,
+    initialValue: {},
+  } as DigiFetchObject;
+  const digimon = useRandomFetch(digiFetchValues);
 
-  console.log(digimon);
   const { name, img, level } = digimon;
 
   return (
@@ -39,7 +28,13 @@ const useEffectProject: NextPage = () => {
       <h3>{level}</h3>
       {img ? (
         <div>
-          <Image alt={"Digimin Pic"} src={img} width={400} height={500}></Image>
+          <Image
+            priority
+            alt={"Digimin Pic"}
+            src={img}
+            width={400}
+            height={500}
+          ></Image>
         </div>
       ) : (
         <div>
