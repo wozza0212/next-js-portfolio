87512f627f8b2eb87a36d540edf392b31b78a290
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-08-10 16:55:32 +0100

    add ids (typescript)

--- my-app/src/components/Tasks/Tasks.tsx
+++ my-app/src/components/Tasks/Tasks.tsx
@@ -1,14 +1,19 @@
 import { useState, ChangeEvent } from "react";
-type Task = string;
-type Tasks = string[];
+import { v4 as uuidv4 } from "uuid";
+type Task = {
+  task: string;
+  id?: string;
+};
+type Tasks = Task[];
 
+let count = 0;
 const Tasks = () => {
-  const [taskText, setTaskText] = useState<Task>("");
+  const [taskText, setTaskText] = useState<Task>({ task: "", id: "" });
   const [tasks, setTasks] = useState<Tasks>([]);
   const [completedTasks, setCompletedTasks] = useState([]);
 
   const updateTaskText = (e: ChangeEvent<HTMLInputElement>) => {
-    setTaskText(e.target.value);
+    setTaskText({ task: e.target.value, id: uuidv4() });
   };
 
   const addTask = () => {
@@ -21,8 +26,18 @@ const Tasks = () => {
     <div>
       <h3>Tasks</h3>
       <div className="form">
-        <input value={taskText} onChange={updateTaskText} />
+        <input value={taskText.task} onChange={updateTaskText} />
         <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="taskList">
+        {tasks.map((task: Task) => {
+          return (
+            <div key={task.id}>
+              <h3>{task.task}</h3>
+              <h3>{task.id}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
