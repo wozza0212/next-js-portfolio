3a198fc62d6c44561e6eae3e6728bb1244573b38
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-08-10 18:56:26 +0100

    wip

--- my-app/src/components/Tasks/Tasks.tsx
+++ my-app/src/components/Tasks/Tasks.tsx
@@ -8,16 +8,21 @@ type Tasks = Task[];
 
 let count = 0;
 const Tasks = () => {
-  const [taskText, setTaskText] = useState<Task>({ task: "", id: "" });
+  const [task, setTaskText] = useState<Task>({ task: "", id: "" });
   const [tasks, setTasks] = useState<Tasks>([]);
-  const [completedTasks, setCompletedTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState<Tasks>([]);
 
   const updateTaskText = (e: ChangeEvent<HTMLInputElement>) => {
     setTaskText({ task: e.target.value, id: uuidv4() });
   };
 
   const addTask = () => {
-    setTasks([...tasks, taskText]);
+    setTasks([...tasks, task]);
+  };
+
+  const completeeTask = (completedTask) => {
+    setCompletedTasks([...completedTasks, completedTask]);
+    setTasks(tasks.filter((task) => task.id !== completedTask.id));
   };
 
   console.log("tasks", tasks);
@@ -26,13 +31,13 @@ const Tasks = () => {
     <div>
       <h3>Tasks</h3>
       <div className="form">
-        <input value={taskText.task} onChange={updateTaskText} />
+        <input value={task.task} onChange={updateTaskText} />
         <button onClick={addTask}>Add Task</button>
       </div>
       <div className="taskList">
         {tasks.map((task: Task) => {
           return (
-            <div key={task.id}>
+            <div key={task.id} onClick={completeeTask}>
               <h3>{task.task}</h3>
               <h3>{task.id}</h3>
             </div>
